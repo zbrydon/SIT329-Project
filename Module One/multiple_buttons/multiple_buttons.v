cc67ce73559785b5396ed944ee9c027697250da5
zbrydon <zacbrydon@gmail.com>
2021-09-05 16:20:05 +1000

    Update multiple_buttons.v

--- Module One/multiple_buttons/multiple_buttons.v
+++ Module One/multiple_buttons/multiple_buttons.v
@@ -1,30 +1,3 @@
-//module button_debouncer(
-//	input button_in,
-//	input clk_in,
-//	input reset,
-//	output button_out
-//);
-//	reg q9, q8, q7, q6, q5, q4, q3, q2, q1, q0;
-//
-//	always @ ( posedge clk_in or posedge reset)
-//
-//	if (reset == 1'b1)
-//		{q9, q8, q7, q6, q5, q4, q3, q2, q1, q0} <= 10'b0;
-//	else begin
-//		q9 <= q8; q8 <= q7; q7 <= q6; q6 <= q5; q5 <= q4;
-//		q4 <= q3; q3 <= q2; q2 <= q1; q1 <= q0; q0 <= button_in;
-//	end
-//	
-//	assign button_out = !q9 & q8 & q7 & q6 & q5 & q4 & q3 & q2 & q1 & q0;
-//
-//endmodule
-
-//	button_debouncer drops_debounce(drops_button, clk, reset, BOARD_LEDs[0]);
-//	button_debouncer hoods_debounce( hoods_button, clk, reset, BOARD_LEDs[1]);
-//	button_debouncer tops_debounce( tops_button, clk, reset, BOARD_LEDs[2]);
-//	button_debouncer bar_debounce( bar_button, clk, reset, BOARD_LEDs[3]);
-//	button_debouncer seat_debounce( seat_button, clk, reset, BOARD_LEDs[4]);
-
 module multiple_buttons(
 	input wire clk,
 	
